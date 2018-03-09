07fbe6819c630f64112fbd44e56a9094a1b04346
llanTang <772894177@qq.com>
2018-03-09 14:01:14 +1030

    Add files via upload

---
+++ gTest/gTest.hpp
@@ -0,0 +1,15 @@
+//
+//  gTest.hpp
+//  ADTree
+//
+//  Created by ustc@tll on 28/2/18.
+//  Copyright © 2018 ustc@tll. All rights reserved.
+//
+
+#ifndef gTest_hpp
+#define gTest_hpp
+
+double gammq(double a, double x);
+
+
+#endif /* gTest_hpp */
