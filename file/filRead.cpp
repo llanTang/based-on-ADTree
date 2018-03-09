8554cfa700c28af78ba4775b0d4fbefd2f756a57
llanTang <772894177@qq.com>
2018-03-09 13:59:03 +1030

    Add files via upload

---
+++ file/filRead.cpp
@@ -0,0 +1,9 @@
+//
+//  filRead.cpp
+//  ADTree
+//
+//  Created by ustc@tll on 6/3/18.
+//  Copyright © 2018 ustc@tll. All rights reserved.
+//
+
+#include "filRead.hpp"
---
+++ file/filRead.hpp
@@ -0,0 +1,14 @@
+//
+//  filRead.hpp
+//  ADTree
+//
+//  Created by ustc@tll on 6/3/18.
+//  Copyright © 2018 ustc@tll. All rights reserved.
+//
+
+#ifndef filRead_hpp
+#define filRead_hpp
+
+#include <stdio.h>
+
+#endif /* filRead_hpp */
